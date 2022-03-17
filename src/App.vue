865c8e9f22e4175ca670fbd0b49adf2209bc7ef6
Asim Shrestha <asimshrestha608@gmail.com>
2022-03-17 16:43:04 +0545

    feat: hide completed tasks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { v4 as uuidv4 } from "uuid";
 
 const newTask = ref("");
 const tasks = ref([]);
+const hideTasks = ref(false);
 
 function addTask() {
   const task = { id: uuidv4(), text: newTask.value, done: false };
@@ -14,6 +15,10 @@ function addTask() {
 function removeTask(task) {
   tasks.value = tasks.value.filter((t) => t !== task);
 }
+
+const filteredTasks = computed(() => {
+  return hideTasks.value ? tasks.value.filter((t) => !t.done) : tasks.value;
+});
 </script>
 
 <template>
@@ -26,13 +31,17 @@ function removeTask(task) {
   </form>
 
   <ul v-if="tasks.length">
-    <li v-for="task in tasks" :key="task.id">
+    <li v-for="task in filteredTasks" :key="task.id">
       <input type="checkbox" :id="task.id" v-model="task.done" />
       <label :for="task.id">{{ task.text }}</label>
       <button class="remove-btn" @click="removeTask(task)">X</button>
     </li>
   </ul>
   <p v-else>No tasks added yet.</p>
+
+  <button @click="hideTasks = !hideTasks" v-if="tasks.length">
+    {{ hideTasks ? "Show all" : "Hide completed tasks" }}
+  </button>
 </template>
 
 <style>
